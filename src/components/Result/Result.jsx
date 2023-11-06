1064848438e444b106bea5b555dba02481e6a32a
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-11-06 16:37:59 +0500

    finished desktop info about repos

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -8,17 +8,46 @@ function Result() {
       <div className="result__container">
         <div className="result__user">
           <div className="result__image">
-            <img src={userInfo} alt="user info" />
+            <img src={userInfo} alt="user info" className="image" />
           </div>
           <div className="result__info">
-            <p>
-              The Octocat <br />
-              @octocat this profile has no bio
-            </p>
-            <p>Joined 25 Jan 2011</p>
+            <div className="result__info-details">
+              <h1 className="result__title">
+                The Octocat
+                <h4 className="result__date">Joined 25 Jan 2011</h4>
+              </h1>
+
+              <a rel="stylesheet" href="/" className="result__account">
+                @octocat
+              </a>
+              <h4 className="result__bio">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+                odio. Quisque volutpat mattis eros.
+              </h4>
+            </div>
           </div>
-          {/* <div className="user__repos">repos 8 followers 3938 following 9</div>
-      <div className="user__links">geo github work twitter</div> */}
+        </div>
+        <div className="result__links">
+          <div className="result__repos">
+            <table>
+              <tr className="table_title">
+                <th>repos</th>
+                <th>followers</th>
+                <th>following</th>
+              </tr>
+              <tr className="table_info">
+                <td>8</td>
+                <td>3938</td>
+                <td>9</td>
+              </tr>
+            </table>
+          </div>
+          <div className="result__link">
+            <a href="/">geo</a>
+            <a href="/">github</a>
+            <a href="/">work</a>
+            <a href="/">twitter</a>
+          </div>
         </div>
       </div>
     </div>
