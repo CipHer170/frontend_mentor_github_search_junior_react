27c8c170ef7db97b037c98b5cd538da479be7468
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2023-11-07 12:52:24 +0500

    links seperated

--- src/components/LinkBar/LinkBar.jsx
+++ src/components/LinkBar/LinkBar.jsx
@@ -1,7 +1,54 @@
 import React from "react";
-
+import {
+  BsFillBuildingsFill,
+  BsFillGeoAltFill,
+  BsLink45Deg,
+  BsTwitter,
+} from "react-icons/bs";
+import "./LinkBar.scss";
 function LinkBar() {
-  return <div>LinkBar</div>;
+  return (
+    <div>
+      {" "}
+      <div className="result__links">
+        <div className="result__repos">
+          <table>
+            <tr className="table_title">
+              <th>repos</th>
+              <th>followers</th>
+              <th>following</th>
+            </tr>
+            <tr className="table_info">
+              <td>8</td>
+              <td>3938</td>
+              <td>9</td>
+            </tr>
+          </table>
+        </div>
+        <div className="result__link">
+          <a href="/">
+            {" "}
+            <BsFillGeoAltFill />
+            San Francisco
+          </a>
+
+          <a href="/">
+            <BsTwitter />
+            Not Available
+          </a>
+          <a href="/">
+            <BsLink45Deg />
+            https://github.blog
+          </a>
+
+          <a href="/">
+            <BsFillBuildingsFill />
+            @github
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LinkBar;
--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import userInfo from "../../assets/Bitmap.svg";
 import "./Result.scss";
-import {
-  BsFillBuildingsFill,
-  BsFillGeoAltFill,
-  BsLink45Deg,
-  BsTwitter,
-} from "react-icons/bs";
+import LinkBar from "../LinkBar/LinkBar";
+
 function Result() {
   return (
     <div className="result">
@@ -32,44 +28,8 @@ function Result() {
             </div>
           </div>
         </div>
-        <div className="result__links">
-          <div className="result__repos">
-            <table>
-              <tr className="table_title">
-                <th>repos</th>
-                <th>followers</th>
-                <th>following</th>
-              </tr>
-              <tr className="table_info">
-                <td>8</td>
-                <td>3938</td>
-                <td>9</td>
-              </tr>
-            </table>
-          </div>
-          <div className="result__link">
-            <a href="/">
-              {" "}
-              <BsFillGeoAltFill />
-              San Francisco
-            </a>
-
-            <a href="/">
-              <BsTwitter />
-              Not Available
-            </a>
-            <a href="/">
-              <BsLink45Deg />
-              https://github.blog
-            </a>
-
-            <a href="/">
-              <BsFillBuildingsFill />
-              @github
-            </a>
-          </div>
-        </div>
       </div>
+      <LinkBar />
     </div>
   );
 }
